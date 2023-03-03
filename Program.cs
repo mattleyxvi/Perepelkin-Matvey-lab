3c572b02a48a171842cbe836821f23cf6ec447fd
mattleyxvi <124852984+mattleyxvi@users.noreply.github.com>
2023-03-03 19:25:37 +0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,51 @@
-﻿
+public class Teacher
+{
+    public string Name;
+    public List<string> Subject;
+    public List<string> Groups;
+    public Teacher()
+    {
+        Console.WriteLine("имя");
+        Name = Console.ReadLine();
+        List<string> Lesns = new List<string>();
+        while (true)
+        {
+            Console.WriteLine("Add more subj?");
+            string a = Console.ReadLine();
+            if (a == "y")
+            {
+                Console.WriteLine("subj");
+                Lesns.Add(Console.ReadLine());
+            }
+            else
+            {
+                Subject = Lesns;
+                break;
+            }
+
+        }
+        List<string> groups = new List<string>();
+        while (true)
+        {
+            Console.WriteLine("Add more group?");
+            string a = Console.ReadLine();
+            if (a == "y")
+            {
+                Console.WriteLine("group");
+                Lesns.Add(Console.ReadLine());
+            }
+            else
+            {
+                Groups = groups;
+                break;
+            }
+
+        }
+    }
+}
     public class Student
     {
         public string Name, Group;
-        public string Age1, Age2, Age3;
         public List<string[]> Subject;
         public Student()
         {
@@ -11,9 +54,6 @@
             List<string[]> Lesns = new List<string[]>();
             Name = input[0];
             Group = input[1];
-            Age1 = input[2];
-            Age2 = input[3];
-            Age3 = input[4];
             while (true)
             {
                 Console.WriteLine("Add more subj?");
@@ -32,84 +72,140 @@
             }
         }
     }
+public class Helper
+{
+    public string Name;
+    public Helper()
+    {
+        Console.WriteLine("name");
+        Name = Console.ReadLine();
+    }
+}
+public class Head
+{
+    public string Name;
+    public Dictionary<string, string> Task;
+    public Head()
+    {
+        Console.WriteLine("имя");
+        Name = Console.ReadLine();
+        while (true)
+        {
+            Console.WriteLine("Add more task?");
+            string a = Console.ReadLine();
+            if (a == "y")
+            {
+                Console.WriteLine("input task and ppl");
+                string[] tsk = { Console.ReadLine(),Console.ReadLine() };
+                Task.Add(tsk[0], tsk[1]);
+            }
+            else
+            {
+                break;
+            }
 
-    class HelloWorld
+        }
+    }
+
+}
+    class Program
     {
         static void Main()
         {
-            bool add = true;
-            List<string> chosen = new List<string>();
-        Console.WriteLine("1 отличники, 2 20лет, 3 долги");
+
+            List<Student> students = new List<Student>();
+            List<Teacher> teachers = new List<Teacher>();
+            List<Helper> helpers = new List<Helper>();
+            List<Head> heads = new List<Head>();
+            while (true)
+            {
+                Console.WriteLine("who");
+                string a = Console.ReadLine();
+            if (a == "1")
+                students.Add(new Student());
+            else if (a == "2")
+                teachers.Add(new Teacher());
+            else if (a == "3")
+                helpers.Add(new Helper());
+            else if (a == "4")
+                heads.Add(new Head());
+            else if (a == "5")
+                break;
+            Console.Clear();
+            }
+            
             var w = Console.ReadLine();
             if (w == "1")
             {
-                while (true)
+            var subj = Console.ReadLine();
+            var grup = Console.ReadLine();
+            var stud  = Console.ReadLine();
+            foreach (Student nam in students)
+            {
+                if (nam.Name == stud)
                 {
-                    Console.WriteLine("new stud?");
-                    var q = Console.ReadLine();
-                    if (q == "n")
+                    for (int i = 0; i < nam.Subject.Count(); i++)
                     {
-                        break;
-                    }
-                    else
-                    {
-                        Student boba = new Student();
-                        foreach (string[] i in boba.Subject)
+                        if (nam.Subject[i][1] == "2")
                         {
-                            if (i[1] != "5")
+                            foreach (Teacher t in teachers)
                             {
-                                add = false;
-                                break;
+                                if ((t.Subject.Contains(nam.Subject[i][0]))&&(t.Groups.Contains(nam.Group)))
+                                {
+                                    Console.Write(t.Name);
+                                    Console.Write(' ' + nam.Subject[i][0]);
+                                }
                             }
                         }
-                        if (add = true)
-                            chosen.Add(boba.Name);
                     }
+                    break;
                 }
+            }
             }
             else if (w == "2")
+        {
+            var subj = Console.ReadLine();
+            var teach = Console.ReadLine();
+            foreach (Teacher t in teachers)
             {
-                while (true)
+                if (t.Name == teach)
                 {
-                    Console.WriteLine("new stud?");
-                    var q = Console.ReadLine();
-                    if (q == "n")
+                    foreach (Student st in students)
                     {
-                        break;
-                    }
-                    else
-                    {
-                        Student boba = new Student();
-                        if (Convert.ToInt32(boba.Age3+boba.Age2+boba.Age1)/20230208 >= 20)
-                            chosen.Add(boba.Name);
-                    }
-                }
-            }
-            else if (w == "3")
-            {
-                while (true)
-                {
-                    Console.WriteLine("new stud?");
-                    var q = Console.ReadLine();
-                    if (q == "n")
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        Student boba = new Student();
-                        foreach (string[] i in boba.Subject)
+                        foreach (string[] sbj in st.Subject)
                         {
-                            if (i[1] == "-1")
+                            if ((sbj[1] == "2") && (sbj[0]==subj))
                             {
-                                chosen.Add(boba.Name);
-                                break;
+                                Console.Write(st.Name);
+                                Console.Write(" " + st.Group);  
                             }
                         }
                     }
                 }
             }
-            foreach (string a in chosen)
-                Console.WriteLine(a);
+        }
+            else if (w == "3")
+        {
+            var task = Console.ReadLine();
+            foreach (Head head in heads)
+            {
+                if (head.Task.ContainsKey(task))
+                {
+                    Console.WriteLine(head.Name);
+                }
+            }
+        }
+            else if (w=="4")
+        {
+            var head = Console.ReadLine();
+            foreach (Head hed in heads)
+            {
+                if (hed.Name == head)
+                {
+                    foreach(string task in hed.Task.Keys)
+                        Console.WriteLine($"{task}");
+                }
+            }
+        }
         }
     }
