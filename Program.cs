9fb984b249f560993f91b4fdb9233011cbc63b45
mattleyxvi <124852984+mattleyxvi@users.noreply.github.com>
2023-03-04 10:22:38 +0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,16 +5,16 @@ public class Teacher
     public List<string> Groups;
     public Teacher()
     {
-        Console.WriteLine("имя");
+        Console.WriteLine("введите имя");
         Name = Console.ReadLine();
         List<string> Lesns = new List<string>();
         while (true)
         {
-            Console.WriteLine("Add more subj?");
+            Console.WriteLine("Добавить предмет? да/нет");
             string a = Console.ReadLine();
-            if (a == "y")
+            if (a == "да")
             {
-                Console.WriteLine("subj");
+                Console.WriteLine("введите предмет");
                 Lesns.Add(Console.ReadLine());
             }
             else
@@ -27,12 +27,12 @@ public class Teacher
         List<string> groups = new List<string>();
         while (true)
         {
-            Console.WriteLine("Add more group?");
+            Console.WriteLine("еще группу? у==да");
             string a = Console.ReadLine();
             if (a == "y")
             {
-                Console.WriteLine("group");
-                Lesns.Add(Console.ReadLine());
+                Console.WriteLine("введите группу");
+                groups.Add(Console.ReadLine());
             }
             else
             {
@@ -43,41 +43,41 @@ public class Teacher
         }
     }
 }
-    public class Student
+public class Student
+{
+    public string Name, Group;
+    public List<string[]> Subject;
+    public Student()
     {
-        public string Name, Group;
-        public List<string[]> Subject;
-        public Student()
-        {
-            Console.WriteLine("имя группа даты");
-            string[] input = Console.ReadLine().Split(" ");
-            List<string[]> Lesns = new List<string[]>();
-            Name = input[0];
-            Group = input[1];
-            while (true)
-            {
-                Console.WriteLine("Add more subj?");
-                string a = Console.ReadLine();
-                if (a == "y")
-                {
-                    Console.WriteLine("subj mark");
-                    Lesns.Add(Console.ReadLine().Split(" "));
-                }
-                else
-                {
-                    Subject = Lesns;
-                    break;
-                }
+        Console.WriteLine("имя группа ");
 
+        List<string[]> Lesns = new List<string[]>();
+        Name = Console.ReadLine();
+        Group = Console.ReadLine();
+        while (true)
+        {
+            Console.WriteLine("Еще предмет добавить? да/нет");
+            string a = Console.ReadLine();
+            if (a == "да")
+            {
+                Console.WriteLine("предмет и оценка");
+                Lesns.Add(Console.ReadLine().Split(" "));
             }
+            else
+            {
+                Subject = Lesns;
+                break;
+            }
+
         }
     }
+}
 public class Helper
 {
     public string Name;
     public Helper()
     {
-        Console.WriteLine("name");
+        Console.WriteLine("имя");
         Name = Console.ReadLine();
     }
 }
@@ -89,18 +89,21 @@ public class Head
     {
         Console.WriteLine("имя");
         Name = Console.ReadLine();
+        Dictionary<string,string> tsk = new Dictionary<string,string>();
         while (true)
         {
-            Console.WriteLine("Add more task?");
+            Console.WriteLine("Добавит приказ? да/нет");
             string a = Console.ReadLine();
-            if (a == "y")
+            if (a == "да")
             {
-                Console.WriteLine("input task and ppl");
-                string[] tsk = { Console.ReadLine(),Console.ReadLine() };
-                Task.Add(tsk[0], tsk[1]);
+                Console.WriteLine("введите приказ а затем для кого он");
+                string q = Console.ReadLine();
+                string qq = Console.ReadLine();
+                tsk[q]=qq;
             }
             else
             {
+                Task = tsk;
                 break;
             }
 
@@ -108,19 +111,19 @@ public class Head
     }
 
 }
-    class Program
+class Program
+{
+    static void Main()
     {
-        static void Main()
-        {
 
-            List<Student> students = new List<Student>();
-            List<Teacher> teachers = new List<Teacher>();
-            List<Helper> helpers = new List<Helper>();
-            List<Head> heads = new List<Head>();
-            while (true)
-            {
-                Console.WriteLine("who");
-                string a = Console.ReadLine();
+        List<Student> students = new List<Student>();
+        List<Teacher> teachers = new List<Teacher>();
+        List<Helper> helpers = new List<Helper>();
+        List<Head> heads = new List<Head>();
+        while (true)
+        {
+            Console.WriteLine("1-студент;2-препод;3-помошник;4-управляющий;5-закончить ввод");
+            string a = Console.ReadLine();
             if (a == "1")
                 students.Add(new Student());
             else if (a == "2")
@@ -132,80 +135,92 @@ public class Head
             else if (a == "5")
                 break;
             Console.Clear();
-            }
+        }
+        
+        while (true)
+        {
+            Console.WriteLine("1-выдать долги для студента;2-выдать должников по предмету для препода;3-выдать автора приказа;4-выдать все приказы одного автора");
             
             var w = Console.ReadLine();
+            Console.Clear();
             if (w == "1")
             {
-            var subj = Console.ReadLine();
-            var grup = Console.ReadLine();
-            var stud  = Console.ReadLine();
-            foreach (Student nam in students)
-            {
-                if (nam.Name == stud)
+                Console.WriteLine("Введите имя студента");
+                string stud = Console.ReadLine();
+                foreach (Student nam in students)
                 {
-                    for (int i = 0; i < nam.Subject.Count(); i++)
+                    if (nam.Name == stud)
                     {
-                        if (nam.Subject[i][1] == "2")
+                        foreach (string[] i in nam.Subject)
                         {
-                            foreach (Teacher t in teachers)
+                            if (i[1] == "2")
                             {
-                                if ((t.Subject.Contains(nam.Subject[i][0]))&&(t.Groups.Contains(nam.Group)))
+                                foreach (Teacher t in teachers)
                                 {
-                                    Console.Write(t.Name);
-                                    Console.Write(' ' + nam.Subject[i][0]);
+                                    if ((t.Subject.Contains(i[0])) && (t.Groups.Contains(nam.Group)))
+                                    {
+                                        Console.Write(t.Name);
+                                        Console.WriteLine(' ' + i[0]);
+                                    }
+                                }
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+            else if (w == "2")
+            {
+                Console.WriteLine("Введите предмет");
+                var subj = Console.ReadLine();
+                Console.WriteLine("Введите имя препода");
+                var teach = Console.ReadLine();
+                foreach (Teacher t in teachers)
+                {
+                    if (t.Name == teach)
+                    {
+                        foreach (Student st in students)
+                        {
+                            foreach (string[] sbj in st.Subject)
+                            {
+                                if ((sbj[1] == "2") && (sbj[0] == subj))
+                                {
+                                    Console.Write(st.Name);
+                                    Console.WriteLine(" " + st.Group);
                                 }
                             }
                         }
                     }
-                    break;
                 }
             }
-            }
-            else if (w == "2")
-        {
-            var subj = Console.ReadLine();
-            var teach = Console.ReadLine();
-            foreach (Teacher t in teachers)
+            else if (w == "3")
             {
-                if (t.Name == teach)
+                Console.WriteLine("Введите имя управленца");
+                var task = Console.ReadLine();
+                foreach (Head head in heads)
                 {
-                    foreach (Student st in students)
+                    if (head.Task.ContainsKey(task))
                     {
-                        foreach (string[] sbj in st.Subject)
-                        {
-                            if ((sbj[1] == "2") && (sbj[0]==subj))
-                            {
-                                Console.Write(st.Name);
-                                Console.Write(" " + st.Group);  
-                            }
-                        }
+                        Console.WriteLine(head.Name);
                     }
                 }
             }
-        }
-            else if (w == "3")
-        {
-            var task = Console.ReadLine();
-            foreach (Head head in heads)
+            else if (w == "4")
             {
-                if (head.Task.ContainsKey(task))
+                Console.WriteLine("Введите имя");
+                var head = Console.ReadLine();
+                foreach (Head hed in heads)
                 {
-                    Console.WriteLine(head.Name);
+                    foreach(string ppl in hed.Task.Keys)
+                        if (hed.Task[ppl] == head)
+                    {
+                            Console.WriteLine($"{ppl}");
+                    }
                 }
             }
-        }
-            else if (w=="4")
-        {
-            var head = Console.ReadLine();
-            foreach (Head hed in heads)
-            {
-                if (hed.Name == head)
-                {
-                    foreach(string task in hed.Task.Keys)
-                        Console.WriteLine($"{task}");
-                }
-            }
-        }
+            else if (w == "5")
+                break;
+            
         }
     }
+}
